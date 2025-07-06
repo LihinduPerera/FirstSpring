94aac33852f6b063228342b77887a6b85141521d
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-07-06 20:45:59 +0530

    Add validation and error handling to Run and RunController classes

--- src/main/java/dev/lihindu/firstspring/run/Run.java
+++ src/main/java/dev/lihindu/firstspring/run/Run.java
@@ -1,14 +1,21 @@
 package dev.lihindu.firstspring.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Run(
         Integer id,
-        String title,
+        @NotEmpty String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
-        Integer miles,
+        @Positive Integer miles,
         Location location
 ) {
-
+    public Run {
+        if(!completedOn.isAfter(startedOn)) {
+            throw new IllegalArgumentException("Completed On must be after Started On");
+        }
+    }
 }
