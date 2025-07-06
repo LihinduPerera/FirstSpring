68b089ccbe8a8f8c792e87608840b63aead65315
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-07-06 18:25:01 +0530

    Refactor main method to include logging and context management

--- src/main/java/dev/lihindu/firstspring/FirstspringApplication.java
+++ src/main/java/dev/lihindu/firstspring/FirstspringApplication.java
@@ -1,13 +1,20 @@
 package dev.lihindu.firstspring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FirstspringApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(FirstspringApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(FirstspringApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(FirstspringApplication.class, args);
+		log.info("Application Started Successfully !");
+
 	}
- 
+
 }
