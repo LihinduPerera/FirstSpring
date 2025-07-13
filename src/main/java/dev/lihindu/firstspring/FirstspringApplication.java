7a89f89f54639f86d853b3ce4247010f8bf1e2d3
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-07-13 21:05:41 +0530

    Implement UserHttpClient methods for retrieving users and update FirstspringApplication to use UserHttpClient

--- src/main/java/dev/lihindu/firstspring/FirstspringApplication.java
+++ src/main/java/dev/lihindu/firstspring/FirstspringApplication.java
@@ -1,9 +1,19 @@
 package dev.lihindu.firstspring;
 
+import dev.lihindu.firstspring.user.User;
+import dev.lihindu.firstspring.user.UserHttpClient;
+import dev.lihindu.firstspring.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @SpringBootApplication
 public class FirstspringApplication {
@@ -14,13 +24,32 @@ public class FirstspringApplication {
 		SpringApplication.run(FirstspringApplication.class, args);
 
 	}
+	@Bean
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
 
 //	@Bean
-//	CommandLineRunner runner(RunRepository runRepository) {
-//		return  args -> {
-//			Run run = new Run(1, "First Run" , LocalDateTime.now() , LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5 , Location.OUTDOOR);
-//			runRepository.create(run);
+//	CommandLineRunner runner(UserRestClient client) {
+//		return args -> {
+//			List<User> users = client.findAll();
+////			System.out.println(users);
+//
+//			User user = client.findById(2);
+//			System.out.println(user);
 //		};
 //	}
 
+	@Bean
+	CommandLineRunner runner(UserHttpClient client) {
+		return args -> {
+			List<User> users = client.findAll();
+	//			System.out.println(users);
+
+			User user = client.findById(2);
+			System.out.println(user);
+		};
+	}
 }
